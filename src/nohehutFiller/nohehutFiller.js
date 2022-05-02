f104639fb4df3d5817ac6bd1787e49bd7d372a6b
avivpolakericom <aviv.polak@ericom.com>
2022-05-02 10:15:04 +0300

    configs

--- src/nohehutFiller/nohehutFiller.js
+++ src/nohehutFiller/nohehutFiller.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const {readConfig} =require("./configReader")
+const { readConfig } = require("./configReader")
 
 function delay(time) {
     return new Promise(function (resolve) {
@@ -8,41 +8,85 @@ function delay(time) {
 }
 const fill = async () => {
     const config = await readConfig("./config.yml")
-   //login
-     const browser = await puppeteer.launch({ headless: false, slowMo: 40 });
-     const page = await browser.newPage();
-     await page.setDefaultNavigationTimeout(100000);
-     await page.goto("https://priority-web.ericom.com/inout.html");
-          await delay(5000);
+    //login
+    const browser = await puppeteer.launch({ headless: false, slowMo: 40 });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(100000);
+    await page.goto("https://priority-web.ericom.com/inout.html");
+    await delay(5000);
 
     await page.type("#NAM", config.username, {
         delay: getRandomNum(),
     });
-    await page.type("#PAS",config.password, {
+    await page.type("#PAS", config.password, {
         delay: getRandomNum(),
     });
     await page.click(
         "body > form > button"
     );
     await delay(5000);
-    await page.click(
+    try {
+            await page.click(
         "body > form > table > tbody > tr:nth-child(2) > td.last2 > font > a"
     );
+    } catch (error) {
+        await page.click(
+            "body > form > table > tbody > tr:nth-child(3) > td.last2 > font > a"
+        );
+        await delay(5000);
+        await page.select('body > form > table:nth-child(2) > tbody > tr > td > div > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2) > font > select', 'ericom')
+        await page.select('body > form > table:nth-child(2) > tbody > tr > td > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(1) > font > select', "2")
+        await page.click(
+            "body > form > table:nth-child(2) > tbody > tr > td > div > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(1) > font > a > img"
+        );
+        await page.click(
+            "body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr > td:nth-child(1) > a > img"
+        );
+
+    }
+
     await delay(5000);
-    //filling the data
-    for(let month in config.Attendance){
-        for(let day in config.Attendance[month]){
-            con
+    // //filling the data
+
+    for (const year in config.Attendance) {
+        for (const month in config.Attendance[year]) {
+            for (const day in config.Attendance[year][month]) {
+                const { start, end } = config.Attendance[year][month][day]
+                const date = `${day}/${month}/${year}`
+                await page.click(
+                    "body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr > td:nth-child(2) > font:nth-child(4) > a > img"
+                );
+                await delay(10000);
+                try {
+                    await page.type("body > form > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > font > input[type=text]", date, {
+                        delay: getRandomNum(),
+                    });
+                } catch (error) {
+                    
+                }
+               
+                // await page.type("body > form > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > font > input[type=text]", start, {
+                //     delay: getRandomNum(),
+                // });
+                // await page.type("body > form > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(3) > td:nth-child(2) > font > input[type=text]", end, {
+                //     delay: getRandomNum(),
+                // });
+                // await page.select('body > form > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(4) > td:nth-child(2) > font > select', "-101")
+
+            }
         }
     }
-    await page.click(
-        "body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr > td:nth-child(1) > font:nth-child(4) > a > img"
-    );
-    await delay(3000);
-    const date = `${}`
+    // await page.click(
+    //     "body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr > td:nth-child(1) > font:nth-child(4) > a > img"
+    // );
+    // await delay(3000);
+    // const date = `${}`
 
 
-   
+    ///.///////
+
+
+
     // await delay(15000);
     // await page.keyboard.press("Escape");
     // await page.click("#tabs > ul > li:nth-child(2)");
